d66c6c5e2888913dcf897ca2bb68f707415a2292
AnshulGarg <anshulgargg06@gmail.com>
2020-09-12 17:49:38 +0530

    Add schema for question bank

---
+++ model/questionBank.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const questionSchema = mongoose.Schema({
+    Query:{
+        type:String,
+        required:true,
+    },
+    Topic:{
+        type:String,
+        required:true,
+        enum: ['qualifying-criteria' , 'top-colleges']
+    },
+    Tags: [{
+        type:String,
+        required:true,
+        enum:['usa', 'engineering' , 'top' , 'admission' , 'standford-university']
+    }],
+},{
+    timestamps:true
+});
+
+const Question = mongoose.model('Question' , questionSchema);
+
+module.exports = Question;
